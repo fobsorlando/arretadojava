c4041ec68731afda4bea8d7eb25342a86d176472
Fobs <orlandosaboia@gmail.com>
2021-04-11 19:21:11 -0300

    Departamento

---
+++ src/application/Programa.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Departamento obj = new Departamento(1,"BOLOS");
+		
+		System.out.println(obj);
+
+	}
+
+}
---
+++ src/model/entities/Departamento.java
@@ -0,0 +1,76 @@
+package model.entities;
+
+import java.io.Serializable;
+
+public class Departamento implements Serializable  {
+	
+	/**
+	 * Orlando Sabóia - FOBS
+	 * Abril/2021
+	 */
+	
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String no_departamento;
+	
+	public Departamento () {
+		
+	}
+
+	public Departamento(Integer id, String no_departamento) {
+		super();
+		this.id = id;
+		this.no_departamento = no_departamento;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "Departamento [id=" + id + ", no_departamento=" + no_departamento + "]";
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Departamento other = (Departamento) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNo_departamento() {
+		return no_departamento;
+	}
+
+	public void setNo_departamento(String no_departamento) {
+		this.no_departamento = no_departamento;
+	}
+	
+}
