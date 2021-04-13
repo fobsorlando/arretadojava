112b7cd9ee877d722c4131563cfe9f56c99cf05b
Orlando Saboia <38674596+fobsorlando@users.noreply.github.com>
2021-04-13 13:17:13 -0300

    Update DepartamentoDaoJDBC.java
    Departamento DAO JDBC implementado

--- src/model/dao/impl/DepartamentoDaoJDBC.java
+++ src/model/dao/impl/DepartamentoDaoJDBC.java
@@ -8,34 +8,171 @@ import model.entities.Departamento;
 public class DepartamentoDaoJDBC implements DepartamentoDao {
 	
 
+
+    private Connection conn;
+
+    public ProdutoDaoJDBC(Connection conn) {
+                this.conn=conn;
+    }
+
 	@Override
 	public void insert(Departamento obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+				st = conn.prepareStatement("insert into departamento "
+								+ "(no_departamento) "
+								+ "values (?) ",
+								Statement.RETURN_GENERATED_KEYS
+								);
+
+				st.setString(1, obj.getNo_departamento());
+
+				int rowsAffected = st.executeUpdate();
+
+				if (rowsAffected > 0) {
+							ResultSet rs = st.getGeneratedKeys();
+							if  (rs.next()) {
+									int id = rs.getInt(1);
+									obj.setId(id);
+							}
+							DB.closeResultSet(rs);
+				}
+				else {
+						throw new DbException ("Erro Inexperado: N├úo  houve Inclus├úo!");
+				}
+
+		}
+		catch (SQLException e) {
+				throw new DbException (e.getMessage());
+		}
+		finally {
+				DB.closeStatement(st);
+		}
+
 		
 	}
 
 	@Override
 	public void udpdate(Departamento obj) {
-		// TODO Auto-generated method stub
+				PreparedStatement st = null;
+		try {
+				st = conn.prepareStatement("update  departamento set "
+								+ "no_departamento = ? "
+								+ "where id = ? "
+								);
+
+				st.setString(1, obj.getNo_departamento());
+				 st.setInt(2,obj.getId());
+
+
+				int rowsAffected = st.executeUpdate();
+
+				if (rowsAffected > 0) {
+					// nada por enquanto
+				}
+				else {
+						throw new DbException ("Departamento não Existe para Alteração!");
+				}
+
+		}
+		catch (SQLException e) {
+				throw new DbException (e.getMessage());
+		}
+		finally {
+				DB.closeStatement(st);
+		}
 		
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+				st = conn.prepareStatement("delete from departamento  "
+								+ "where id = ?"
+								);
+
+				st.setInt(1,id);
+
+				int rowsAffected = st.executeUpdate();
+
+				if (rowsAffected == 0 ) {
+						throw  new DbException("ID N├âO EXITE PARA EXCLUIR!");
+				}
+
+		}
+		catch (SQLException e) {
+				throw new DbException (e.getMessage());
+		}
+		finally {
+				DB.closeStatement(st);
+		}
+
 		
 	}
 
 	@Override
 	public Departamento findByid(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+				st = conn.prepareStatement(
+								"select * from departamento  "
+								+ "where produto.id = ?"
+								);
+				st.setInt(1, id);
+				rs = st.executeQuery();
+				if (rs.next()) {
+					Departamento obj = instantiateDepartamento(rs);
+
+					return obj;
+				}
+				return null;
+		}
+		catch (SQLException e){
+				throw new  DbException(e.getMessage());
+		}
+		finally {
+				DB.closeStatement(st);
+				DB.closeResultSet(rs);
+		}
+
+
 	}
 
 	@Override
 	public List<Departamento> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+				st = conn.prepareStatement(
+								"select * from departamento  "
+								);
+				st.setInt(1, id);
+				rs = st.executeQuery();
+				if (rs.next()) {
+					Departamento obj = instantiateDepartamento(rs);
+
+					return obj;
+				}
+				return null;
+		}
+		catch (SQLException e){
+				throw new  DbException(e.getMessage());
+		}
+		finally {
+				DB.closeStatement(st);
+				DB.closeResultSet(rs);
+		}
+
 	}
+
+	private Departamento instantiateDepartamento(ResultSet rs) throws SQLException {
+		Departamento forn = new Departamento();
+		forn.setId(rs.getInt("id_Departamento"));
+		forn.setNo_Departamento(rs.getString("no_Departamento"));
+		return forn;
+	}
+
 
 }
